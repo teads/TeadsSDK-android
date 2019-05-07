deac1c38dfbd22931fa501d86ff32c27d157ecb7
Hugo Gresse <hugo.gresse@teads.tv>
2019-05-07 10:20:15 +0200

    Add Kotlin + update gradle (#128)
    * Add Kotlin + update gradle
    
    * Fix customtabs dependency issue

---
+++ TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/MainFragment.kt
@@ -0,0 +1,19 @@
+package tv.teads.teadssdkdemo
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Empty fragment helping opening the navigation drawer
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
