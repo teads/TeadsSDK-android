770f4bc5315fe441250c5ffd0e369993dacf38f8
benjamin volland <benjamin.volland@teads.tv>
2019-07-08 16:17:19 +0200

    remove useless import

--- TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/format/example/InReadWebViewWrapFragment.kt
+++ TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/format/example/InReadWebViewWrapFragment.kt
@@ -2,7 +2,6 @@ package tv.teads.teadssdkdemo.format.example
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
