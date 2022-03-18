924e98035a85f9dea73330a8165dd71f6868387b
Maxime Liege <maxime.liege@teads.tv>
2022-03-18 12:23:39 +0100

    Sample for AppLovin mediation

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ repositories{
     }
 }
 dependencies {
-    implementation("tv.teads.sdk.android:sdk:5.0.10@aar") {
+    implementation("tv.teads.sdk.android:sdk:5.0.12@aar") {
         transitive = true
     }
 }
---
+++ TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/format/mediation/applovin/AppLovinRecyclerViewFragment.kt
@@ -0,0 +1,38 @@
+package tv.teads.teadssdkdemo.format.mediation.applovin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_inread_recyclerview.*
+import tv.teads.teadssdkdemo.R
+import tv.teads.teadssdkdemo.format.mediation.adapter.AdMobRecyclerViewAdapter
+import tv.teads.teadssdkdemo.format.mediation.adapter.AppLovinRecyclerViewAdapter
+import tv.teads.teadssdkdemo.format.mediation.identifier.AdMobIdentifier
+import tv.teads.teadssdkdemo.utils.BaseFragment
+
+/**
+ * Display inRead as Banner within a RecyclerView using AdMob Mediation.
+ */
+class AppLovinRecyclerViewFragment : BaseFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_inread_recyclerview, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setRecyclerViewAdapter(recyclerView)
+    }
+
+    private fun setRecyclerViewAdapter(recyclerView: RecyclerView) {
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        val adUnit = AdMobIdentifier.getAdUnitFromPid(pid)
+
+        recyclerView.adapter = AppLovinRecyclerViewAdapter(adUnit, context, getTitle())
+    }
+
+    override fun getTitle(): String = "InRead AdMob RecyclerView"
+}
