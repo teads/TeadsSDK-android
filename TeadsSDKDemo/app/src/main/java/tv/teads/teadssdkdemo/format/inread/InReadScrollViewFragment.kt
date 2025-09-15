86ec94fd3dbf275618ad5302435d60bdfec2dc1b
Lucas Bonafé <lucas.bonafe@teads.com>
2025-09-15 14:26:37 +0200

    Implement combined sdk

--- TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/format/inread/InReadScrollViewFragment.kt
+++ TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/format/inread/InReadScrollViewFragment.kt
@@ -6,18 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import tv.teads.sdk.AdOpportunityTrackerView
 import tv.teads.sdk.AdPlacementSettings
-import tv.teads.sdk.AdRatio
 import tv.teads.sdk.AdRequestSettings
 import tv.teads.sdk.InReadAdPlacement
-import tv.teads.sdk.InReadAdViewListener
 import tv.teads.sdk.TeadsSDK
-import tv.teads.sdk.VideoPlaybackListener
+import tv.teads.sdk.combinedsdk.TeadsAdPlacementEventName
+import tv.teads.sdk.combinedsdk.adplacement.TeadsAdPlacementMedia
+import tv.teads.sdk.combinedsdk.adplacement.config.TeadsAdPlacementMediaConfig
+import tv.teads.sdk.combinedsdk.adplacement.interfaces.TeadsAdPlacementEventsDelegate
+import tv.teads.sdk.combinedsdk.adplacement.interfaces.core.TeadsAdPlacement
 import tv.teads.sdk.renderer.InReadAdView
 import tv.teads.teadssdkdemo.R
 import tv.teads.teadssdkdemo.databinding.FragmentInreadScrollviewBinding
-import tv.teads.teadssdkdemo.format.inread.extensions.resizeAdContainer
 import tv.teads.teadssdkdemo.utils.BaseFragment
 
 /**
@@ -45,56 +45,77 @@ class InReadScrollViewFragment : BaseFragment() {
             .enableDebug()
             .build()
 
+        val config = TeadsAdPlacementMediaConfig(pid = pid)
+
         // 2. Create the InReadAdPlacement
         adPlacement = TeadsSDK.createInReadPlacement(requireActivity(), pid, placementSettings)
+
+        val placement = TeadsAdPlacementMedia(
+            requireContext(), config, object : TeadsAdPlacementEventsDelegate {
+                override fun onPlacementEvent(
+                    placement: TeadsAdPlacement<*, *>,
+                    event: TeadsAdPlacementEventName,
+                    data: Map<String, Any>?
+                ) {
+                    Log.d("onPlacementEvent", "$event $data")
+                }
+
+            }
+        )
+
+
 
         // 3. Request the ad and listen its events
         val requestSettings = AdRequestSettings.Builder()
             .pageSlotUrl("http://teads.com")
             .build()
-        adPlacement.requestAd(requestSettings,
-            object : InReadAdViewListener {
-                override fun onAdReceived(ad: InReadAdView, adRatio: AdRatio) {
-                    // Clean and init inReadAdView
-                    inReadAdView?.clean()
-                    inReadAdView = ad
-                    // Add ad to the container and resize
-                    binding.adSlotContainer.addView(ad)
-                    binding.adSlotContainer.resizeAdContainer(adRatio)
-                }
 
-                override fun adOpportunityTrackerView(trackerView: AdOpportunityTrackerView) {
-                    binding.adSlotContainer.addView(trackerView)
-                }
+//        adPlacement.requestAd(requestSettings,
+//            object : InReadAdViewListener {
+//                override fun onAdReceived(ad: InReadAdView, adRatio: AdRatio) {
+//                     Clean and init inReadAdView
+//                    inReadAdView?.clean()
+//                    inReadAdView = ad
+//                     Add ad to the container and resize
+//                    binding.adSlotContainer.addView(ad)
+//                    binding.adSlotContainer.resizeAdContainer(adRatio)
+//                }
+//
+//                override fun adOpportunityTrackerView(trackerView: AdOpportunityTrackerView) {
+//                    binding.adSlotContainer.addView(trackerView)
+//                }
+//
+//                override fun onAdRatioUpdate(adRatio: AdRatio) {
+//                     Resize the ad container
+//                    binding.adSlotContainer.resizeAdContainer(adRatio)
+//                }
+//
+//                override fun onAdClicked() {}
+//                override fun onAdClosed() {}
+//                override fun onAdError(code: Int, description: String) {}
+//                override fun onAdImpression() {}
+//                override fun onAdExpandedToFullscreen() {}
+//                override fun onAdCollapsedFromFullscreen() {}
+//                override fun onFailToReceiveAd(failReason: String) {}
+//            },
+//            object : VideoPlaybackListener {
+//                override fun onVideoComplete() {
+//                    Log.d("PlaybackEvent", "complete")
+//                }
+//
+//                override fun onVideoPause() {
+//                    Log.d("PlaybackEvent", "pause")
+//                }
+//
+//                override fun onVideoPlay() {
+//                    Log.d("PlaybackEvent", "play")
+//                }
+//
+//            }
+//        )
 
-                override fun onAdRatioUpdate(adRatio: AdRatio) {
-                    // Resize the ad container
-                    binding.adSlotContainer.resizeAdContainer(adRatio)
-                }
-
-                override fun onAdClicked() {}
-                override fun onAdClosed() {}
-                override fun onAdError(code: Int, description: String) {}
-                override fun onAdImpression() {}
-                override fun onAdExpandedToFullscreen() {}
-                override fun onAdCollapsedFromFullscreen() {}
-                override fun onFailToReceiveAd(failReason: String) {}
-            },
-            object : VideoPlaybackListener {
-                override fun onVideoComplete() {
-                    Log.d("PlaybackEvent", "complete")
-                }
-
-                override fun onVideoPause() {
-                    Log.d("PlaybackEvent", "pause")
-                }
-
-                override fun onVideoPlay() {
-                    Log.d("PlaybackEvent", "play")
-                }
-
-            }
-        )
+        val adView = placement.loadAd()
+        binding.adSlotContainer.addView(adView)
     }
 
     override fun onDestroy() {
--- TeadsSDKDemo/buildSrc/build.gradle.kts
+++ TeadsSDKDemo/buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:8.5.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
 }
