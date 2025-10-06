af444913312d596f2b0eac3b656afb57d6171328
Lucas Bonafé <lucas.bonafe@teads.com>
2025-10-06 17:16:36 +0200

    wip

--- TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/v6/ui/compose/MediaColumnScreen.kt
+++ TeadsSDKDemo/app/src/main/java/tv/teads/teadssdkdemo/v6/ui/compose/MediaColumnScreen.kt
@@ -1,5 +1,7 @@
 package tv.teads.teadssdkdemo.v6.ui.compose
 
+import android.util.Log
+import android.view.ViewGroup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,11 +9,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import tv.teads.sdk.TeadsSDK
+import tv.teads.sdk.combinedsdk.TeadsAdPlacementEventName
+import tv.teads.sdk.combinedsdk.adplacement.TeadsAdPlacementMedia
+import tv.teads.sdk.combinedsdk.adplacement.config.TeadsAdPlacementMediaConfig
+import tv.teads.sdk.combinedsdk.adplacement.interfaces.TeadsAdPlacementEventsDelegate
+import tv.teads.sdk.combinedsdk.adplacement.interfaces.core.TeadsAdPlacement
 import tv.teads.teadssdkdemo.R
+import tv.teads.teadssdkdemo.v6.data.DemoSessionConfiguration
 import tv.teads.teadssdkdemo.v6.ui.base.components.AdContainer
 import tv.teads.teadssdkdemo.v6.ui.base.components.ArticleBody
 import tv.teads.teadssdkdemo.v6.ui.base.components.ArticleImage
@@ -22,6 +39,49 @@ import tv.teads.teadssdkdemo.v6.ui.base.components.ArticleTitle
 fun MediaColumnScreen(
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+    var adView by remember { mutableStateOf<ViewGroup?>(null) }
+    var mediaAd by remember { mutableStateOf<TeadsAdPlacementMedia?>(null) }
+
+    // Ad loading logic
+    LaunchedEffect(Unit) {
+        // 1. Init configuration
+        TeadsSDK.testMode = true
+
+        val config = TeadsAdPlacementMediaConfig(
+            pid = DemoSessionConfiguration.getPlacementIdOrDefault().toInt(), // Your unique placement id
+            articleUrl = DemoSessionConfiguration.getArticleUrlOrDefault().toUri() // Your article url
+        )
+
+        // 2. Create placement
+        mediaAd = TeadsAdPlacementMedia(
+            context = context,
+            config = config,
+            delegate = object : TeadsAdPlacementEventsDelegate {
+                override fun onPlacementEvent(
+                    placement: TeadsAdPlacement<*, *>,
+                    event: TeadsAdPlacementEventName,
+                    data: Map<String, Any>?
+                ) {
+                    // 5. Stay tuned to lifecycle events
+                    Log.d("MediaColumnScreen", "$placement - $event: $data")
+                }
+            }
+        )
+
+        // 3. Request ad
+        val loadedAdView = mediaAd?.loadAd()
+        adView = loadedAdView
+    }
+
+    // Cleanup when composable is disposed
+    DisposableEffect(Unit) {
+        onDispose {
+            // 4. Clean from memory
+            mediaAd?.clean()
+        }
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -40,9 +100,10 @@ fun MediaColumnScreen(
         ArticleSpacing()
         ArticleBody(text = stringResource(R.string.article_template_body_c))
         ArticleSpacing()
-        AdContainer(
-            // Ad view
-        )
+        
+        // 5. Add in the ad container
+        AdContainer(adView = adView)
+        
         ArticleSpacing()
         ArticleBody(text = stringResource(R.string.article_template_body_d))
     }
